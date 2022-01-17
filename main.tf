bbfd2660b903893c916229a349b5593f73d013bc
Talluri Venkata Naga Lakshmi <88710790+nagalakshmi-rajesh@users.noreply.github.com>
2022-01-17 13:46:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "ec2_instance" {
-    ami = "${var.ami_id}"
-    instance_type = "${var.instance_type}"
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags={
+        Name=var.name
 } 
+}
