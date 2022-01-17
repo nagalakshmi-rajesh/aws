75fab6165fba685246e2507d15128ff0710df7ab
Talluri Venkata Naga Lakshmi <88710790+nagalakshmi-rajesh@users.noreply.github.com>
2022-01-17 13:48:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "ami_id" {
         description = "The AMI to use"
         default = "ami-066333d9c572b0680"
 }
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "test"
+}
