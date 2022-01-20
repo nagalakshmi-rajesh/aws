588832691f6a1187ccd82b1cd0e5441a78d4863d
Talluri Venkata Naga Lakshmi <88710790+nagalakshmi-rajesh@users.noreply.github.com>
2022-01-20 12:52:23 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-west-2"
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags={
+        Name=var.name
+} 
+}
