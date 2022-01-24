42c1e46efd777ba281bca4b983a82c269e154b8b
Talluri Venkata Naga Lakshmi <88710790+nagalakshmi-rajesh@users.noreply.github.com>
2022-01-24 17:01:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "ec2_instance" {
     ami = var.ami_id
     instance_type = var.instance_type
-    count=0
     count=var.count
     tags={
         Name=var.name
